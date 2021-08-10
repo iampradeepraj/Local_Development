27534cff9a47132d81d6fdaa748e433cd294b418
Pradeep R <pradeepraj2494@gmail.com>
2021-08-10 19:19:20 +0530

    Code Checkin 10-Aug-2021

---
+++ PySpark/Python/ZTM/list_Slicing.py
@@ -0,0 +1,13 @@
+amazon_cart = ["Apple","Banana","Carrot"]
+
+amazon_cart[0] = "Dog"
+
+print(amazon_cart)
+
+new_cart = amazon_cart
+
+new_cart[0] = "Tiger"
+
+print(new_cart)
+
+print(amazon_cart)
